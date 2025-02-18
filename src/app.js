330a3703346050a5c63c99ecad1868f1f20c649e
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-18 18:57:45 -0400

    solution 1 mysql error

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 const app = express();
-import { configDotenv } from 'dotenv';
 import { pool } from './db.js';
-configDotenv();
 app.get('/ping', async (req, res) => { 
     const [result] = await pool.query('SELECT "pong" as result');
     res.json(result[0]);
--- src/db.js
+++ src/db.js
@@ -1,8 +1,16 @@
 import { createPool } from "mysql2/promise";
+import {
+  DB_DATABASE,
+  DB_HOST,
+  DB_PASS,
+  DB_PORT,
+  DB_USER,
+} from "./config.js";
+
 export const pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASS,
+  port: DB_PORT,
+  database: DB_DATABASE,
 });
