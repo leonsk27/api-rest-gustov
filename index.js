5ed369b6723e0e256d60e3762f5930804862bc17
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-19 05:12:05 -0400

    force sync models

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import app from "./src/app.js";
 import { PORT } from "./src/config.js";
 import sequelize from "./src/models/config.js";
+
+import Food from "./src/models/food-model.js";
+import Sale from "./src/models/sale-model.js";
+import SaleDetail from "./src/models/sale-detail-model.js";
+
 const startServer = async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-    await sequelize.sync({force: true}); // Sync all models
+    // await sequelize.sync({force: true}); // Sync all models
+    await Food.sync({ force: true });
+    await Sale.sync({ force: true });
+    await SaleDetail.sync({ force: true });
     app.listen(PORT, () => {
       console.log(`Server is running on ${PORT}`);
     });
