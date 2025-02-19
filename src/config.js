af3b9f61dc46098b6381978909ece48826d4a3f7
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-18 20:07:03 -0400

    solution 2: mysql

--- src/config.js
+++ src/config.js
@@ -1,6 +1,14 @@
 import { configDotenv } from 'dotenv';
 configDotenv();
 
+console.log(process.env.PORT);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+console.log(process.env.DB_DATABASE);
+console.log(process.env.DB_PORT);
+
+
 export const PORT = process.env.PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
