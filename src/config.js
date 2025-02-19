64dabe8e8192dc88f6f20a49970a22d4197e7957
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-19 05:30:33 -0400

    force sync models

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ import { config } from 'dotenv';
 config();
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
+export const DB_HOST = process.env.DB_HOST || "mysql-zspc.railway.internal";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASS = process.env.DB_PASS || "SvvwhSyXfrjuQdWJvomWCmTpyfooBTtO";
+export const DB_PASS = process.env.DB_PASS || "mcoeheCJPpXQFngjXgFpVaPVsSfjpayR";
 export const DB_DATABASE = process.env.DB_DATABASE || "railway";
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DATABASE_URL = process.env.DATABASE_URL || `mysql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
