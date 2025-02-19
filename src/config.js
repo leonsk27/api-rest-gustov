f5f458db92af21073a5e0736e95da2fa0d3da0e3
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-18 20:12:32 -0400

    solution 2: mysql

--- src/config.js
+++ src/config.js
@@ -10,8 +10,8 @@ console.log(process.env.DB_PORT);
 
 
 export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_HOST = process.env.DB_HOST || "mysql.railway.internal";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASS = process.env.DB_PASS || "root";
-export const DB_DATABASE = process.env.DB_DATABASE || "db";
+export const DB_PASS = process.env.DB_PASS || "SvvwhSyXfrjuQdWJvomWCmTpyfooBTtO";
+export const DB_DATABASE = process.env.DB_DATABASE || "railway";
 export const DB_PORT = process.env.DB_PORT || 3306;
