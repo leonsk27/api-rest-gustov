4f8d180bee0d47b03f028280fd1aef6482f3b15a
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-20 16:42:14 -0400

    por fin se soluciono: last commit

--- src/controllers/food-controller.js
+++ src/controllers/food-controller.js
@@ -1,5 +1,17 @@
 import Food from '../models/food-model.js';
-
+export const getFoodById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const food = await Food.findOne({where: {id}});
+    if (food) {
+      res.status(200).json(food);
+    } else {
+      res.status(404).json({ error: 'Food not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
 export const getFoods = async (req, res) => {
   try {
     const foods = await Food.findAll();
--- src/routes/food-router.js
+++ src/routes/food-router.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getFoods, addFood, updateFood, getMenu } from '../controllers/food-controller.js';
+import { getFoods, addFood, updateFood, getMenu, getFoodById } from '../controllers/food-controller.js';
 
 const foodRouter = express.Router();
 
 foodRouter.get('/foods', getFoods);
+foodRouter.get('/foods/:id', getFoodById);
 foodRouter.post('/foods', addFood);
 foodRouter.patch('/foods/:id', updateFood);
 foodRouter.get('/foods/menu', getMenu);
