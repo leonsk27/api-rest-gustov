0e9dcde2ef1fd18d372cc42871c85a97459d1d8a
Carlos Ojeda <carlos.ojeda.a.v@gmail.com>
2025-02-20 09:17:08 -0400

    add route: correcciones

--- src/routes/food-router.js
+++ src/routes/food-router.js
@@ -6,5 +6,5 @@ const foodRouter = express.Router();
 foodRouter.get('/foods', getFoods);
 foodRouter.post('/foods', addFood);
 foodRouter.patch('/foods/:id', updateFood);
-foodRouter,get('/foods/menu', getMenu);
+foodRouter.get('/foods/menu', getMenu);
 export default foodRouter;
